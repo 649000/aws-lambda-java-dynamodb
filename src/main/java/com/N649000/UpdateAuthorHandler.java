0847ea10d2a3afc0703191f394c4624ef4ec49b1
Nazri <649000@users.noreply.github.com>
2022-02-21 17:41:31 +0800

    Implementation for update author

--- src/main/java/com/N649000/UpdateAuthorHandler.java
+++ src/main/java/com/N649000/UpdateAuthorHandler.java
@@ -5,14 +5,19 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
 
-public class UpdateAuthorHandler  implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+import java.util.HashMap;
+
+public class UpdateAuthorHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private Regions REGION = Regions.AP_SOUTHEAST_1;
 
     @Override
@@ -27,9 +32,29 @@ public class UpdateAuthorHandler  implements RequestHandler<APIGatewayProxyReque
                 .withRegion(REGION)
                 .build();
         DynamoDBMapper mapper = new DynamoDBMapper(client);
-        mapper.save(requestEvent.getBody());
-//        mapper.save(user, new DynamoDBMapperConfig(SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES));
+        Author author = gson.fromJson(requestEvent.getBody(), Author.class);
+        final String id = requestEvent.getPathParameters().get("id");
 
-        return null;
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+
+        try {
+            mapper.save(author,
+                    DynamoDBMapperConfig.builder()
+                            .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES)
+                            .build());
+            responseEvent.setStatusCode(HttpStatus.SC_OK);
+            Author newAuthor = mapper.load(Author.class, id);
+            responseEvent.setBody(gson.toJson(newAuthor));
+        } catch (Exception e) {
+            logger.log(e.getMessage());
+            responseEvent.setStatusCode(HttpStatus.SC_BAD_REQUEST);
+        }
+
+        //Setting headers
+        HashMap<String, String> header = new HashMap<>();
+        header.put(HttpHeaders.CONTENT_TYPE, "application/json");
+        responseEvent.setHeaders(header);
+
+        return responseEvent;
     }
 }
