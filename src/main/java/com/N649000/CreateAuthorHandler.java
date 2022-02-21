0f17dcc88c96a707ae448df142fafa4813f30de5
Nazri <649000@users.noreply.github.com>
2022-02-21 17:03:35 +0800

    Updated CreateAuthorHandler to save new Author into db

--- src/main/java/com/N649000/CreateAuthorHandler.java
+++ src/main/java/com/N649000/CreateAuthorHandler.java
@@ -1,41 +1,75 @@
 package com.N649000;
 
 
+import com.N649000.models.Author;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAuthorHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private Regions REGION = Regions.AP_SOUTHEAST_1;
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        //Reference: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBMapper.CRUDExample1.html
         LambdaLogger logger = context.getLogger();
-        logger.log("APIGatewayProxyRequestEvent::" +requestEvent.toString());
+        Gson gson = new Gson();
+        logger.log("APIGatewayProxyRequestEvent::" + requestEvent.toString());
+
+
+        //Initializing db settings
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(REGION)
+                .build();
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+
+
+        Author author = gson.fromJson(requestEvent.getBody(), Author.class);
+
+        // Set expected false for an attribute -  Save only if did not exist
+        ExpectedAttributeValue expectedAttributeValue = new ExpectedAttributeValue();
+        expectedAttributeValue.setExists(Boolean.FALSE);
+
+        // Map the id field to the ExpectedAttributeValue
+        Map<String, ExpectedAttributeValue> expectedAttributes = new HashMap<>();
+        expectedAttributes.put("id", expectedAttributeValue);
+
+        // Use the attributes within a DynamoDBSaveExpression
+        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression();
+        saveExpression.setExpected(expectedAttributes);
+
 
         APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
-        responseEvent.setBody(requestEvent.getBody());
 
-        //Retrieves HTTP body
-        logger.log("requestEvent.getBody()::" +requestEvent.getBody());
+        try {
+            // Save to dynamoDBMapper using the saveExpression
+            mapper.save(author, saveExpression);
+            responseEvent.setBody(gson.toJson(author));
+            responseEvent.setStatusCode(HttpStatus.SC_CREATED);
+        } catch (ConditionalCheckFailedException e){
+            responseEvent.setStatusCode(HttpStatus.SC_CONFLICT);
+        }
 
-        //Save if did not exist
-//        User newUser = new User();
-//        newUser.setUsername(username);
-//        newUser.setPassword(password);
-//
-//        DynamoDBSaveExpression saveExpr = new DynamoDBSaveExpression();
-//        saveExpr.setExpected(new ImmutableMap.Builder()
-//                .put("username", new ExpectedAttributeValue(false)).build());
-//
-//        dynamoDBMapper.save(newUser, saveExpr);
+        //Setting headers
+        HashMap<String, String> header = new HashMap<>();
+        header.put(HttpHeaders.CONTENT_TYPE, "application/json");
+        responseEvent.setHeaders(header);
 
-        JsonObject convertedObject = new Gson().fromJson(requestEvent.getBody(), JsonObject.class);
-        responseEvent.setStatusCode(convertedObject.get("statusCode").getAsInt());
-        responseEvent.setHeaders(requestEvent.getHeaders());
-        responseEvent.setIsBase64Encoded(requestEvent.getIsBase64Encoded());
         return responseEvent;
     }
 }
