7cba157c7cc7d6100fc29b7f071daaa093958e46
Nazri <649000@users.noreply.github.com>
2022-02-21 17:41:52 +0800

    Skeletal implementation for FindAll

--- src/main/java/com/N649000/FindAllAuthorHandler.java
+++ src/main/java/com/N649000/FindAllAuthorHandler.java
@@ -1,4 +1,29 @@
 package com.N649000;
 
-public class FindAllAuthorHandler {
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+
+public class FindAllAuthorHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private Regions REGION = Regions.AP_SOUTHEAST_1;
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        Gson gson = new Gson();
+        LambdaLogger logger = context.getLogger();
+        logger.log("APIGatewayProxyRequestEvent::" + requestEvent.toString());
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(REGION)
+                .build();
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+        return null;
+    }
 }
